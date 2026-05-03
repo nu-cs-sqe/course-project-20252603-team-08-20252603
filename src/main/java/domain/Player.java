0c335375a408193b5093da22717d89774c550cb7
MengxiXia <mx101@scarletmail.rutgers.edu>
2026-05-03 14:10:03 -0500

    finish player setup phase playertest

--- src/main/java/domain/Player.java
+++ src/main/java/domain/Player.java
@@ -1,4 +1,27 @@
 package domain;
 
+import java.util.Collections;
+import java.util.List;
+
 public class Player {
+
+    public int getPrestigePoints() {
+        return -1;
+    }
+
+    public int getTokenCount(TokenColor color) {
+        return -1;
+    }
+
+    public List<Card> getDevelopmentCards() {
+        return Collections.singletonList(new Card());
+    }
+
+    public List<Card> getReservedCards() {
+        return Collections.singletonList(new Card());
+    }
+
+    public int getTotalTokenCount() {
+        return -1;
+    }
 }
